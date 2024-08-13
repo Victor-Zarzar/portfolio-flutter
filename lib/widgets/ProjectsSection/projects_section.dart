77b0b57f666fd483997e81d01e3bb9fa00668da3
victorzarzar <victorzarzar58@gmail.com>
2024-08-13 14:36:15 -0300

    Feat: add projects section

---
+++ lib/widgets/ProjectsSection/projects_section.dart
@@ -0,0 +1,71 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:portfolio_flutter/constants/Colors/colors.dart';
+import 'package:portfolio_flutter/utils/ProjectsUtils/projects_utils.dart';
+import 'package:portfolio_flutter/widgets/ProjectsCard/projects_card.dart';
+
+class ProjectsSection extends StatelessWidget {
+  const ProjectsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: screenWidth,
+      padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+      child: Column(
+        children: [
+          // Work projects title
+          Text(
+            "workprojects".tr(),
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: CustomColor.whitePrimary,
+            ),
+          ),
+          const SizedBox(height: 50),
+          // Work projects cards
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 900),
+            child: Wrap(
+              spacing: 25,
+              runSpacing: 25,
+              children: [
+                for (int i = 0; i < workProjectUtils.length; i++)
+                  ProjectCardWidget(
+                    project: workProjectUtils[i],
+                  ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 80),
+          // Hobby projects title
+          Text(
+            "hobbyprojects".tr(),
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: CustomColor.whitePrimary,
+            ),
+          ),
+          const SizedBox(height: 50),
+          // Hobby projects cards
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 900),
+            child: Wrap(
+              spacing: 25,
+              runSpacing: 25,
+              children: [
+                for (int i = 0; i < hobbyProjectUtils.length; i++)
+                  ProjectCardWidget(
+                    project: hobbyProjectUtils[i],
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
