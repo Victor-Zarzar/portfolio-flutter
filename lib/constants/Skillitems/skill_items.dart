6006ee4e5a40cd7cef5b7995523bee681666c3ba
victorzarzar <victorzarzar58@gmail.com>
2024-08-13 14:26:27 -0300

    Feat: add skills section

---
+++ lib/constants/Skillitems/skill_items.dart
@@ -0,0 +1,97 @@
+const List<Map> platformItems = [
+  {
+    "img": "assets/android_icon.png",
+    "title": "Android Dev",
+  },
+  {
+    "img": "assets/web_icon.png",
+    "title": "Web Dev",
+  },
+  {
+    "img": "assets/ios_icon.png",
+    "title": "IOS Dev",
+  },
+  {
+    "img": "assets/desktop_icon.png",
+    "title": "Desktop Dev",
+  }
+];
+
+const List<Map> skillItems = [
+  {
+    "img": "assets/react.png",
+    "title": "React",
+  },
+  {
+    "img": "assets/typescript.png",
+    "title": "TypeScript",
+  },
+  {
+    "img": "assets/javascript.png",
+    "title": "JavaScript",
+  },
+  {
+    "img": "assets/nextjs.png",
+    "title": "NextJS",
+  },
+  {
+    "img": "assets/tailwind.png",
+    "title": "TailwindCSS",
+  },
+  {
+    "img": "assets/html5.png",
+    "title": "HTML5",
+  },
+  {
+    "img": "assets/css3.png",
+    "title": "CSS3",
+  },
+  {
+    "img": "assets/docker.png",
+    "title": "Docker",
+  },
+  {
+    "img": "assets/postman.png",
+    "title": "Postman",
+  },
+  {
+    "img": "assets/swagger.png",
+    "title": "Swagger",
+  },
+  {
+    "img": "assets/python.png",
+    "title": "Python",
+  },
+  {
+    "img": "assets/django.png",
+    "title": "Django",
+  },
+  {
+    "img": "assets/flutter.png",
+    "title": "Flutter",
+  },
+  {
+    "img": "assets/dart.png",
+    "title": "Dart",
+  },
+  {
+    "img": "assets/firebase.png",
+    "title": "Firebase",
+  },
+  {
+    "img": "assets/git.png",
+    "title": "Git",
+  },
+  {
+    "img": "assets/gitlab.png",
+    "title": "GitLab",
+  },
+  {
+    "img": "assets/github.png",
+    "title": "GitHub",
+  },
+  {
+    "img": "assets/archlinux.png",
+    "title": "ArchLinux",
+  }
+];
