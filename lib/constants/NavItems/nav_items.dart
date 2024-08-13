a1a185214d90dc13c1795e17632f1f124d9613f4
victorzarzar <victorzarzar58@gmail.com>
2024-08-13 14:21:43 -0300

    Feat: add Navitems

---
+++ lib/constants/NavItems/nav_items.dart
@@ -0,0 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+List<String> navTitles = [
+  'home'.tr(),
+  'skills'.tr(),
+  'projects'.tr(),
+  'contact'.tr(),
+];
+
+List<IconData> navIcons = [
+  Icons.home,
+  Icons.handyman_outlined,
+  Icons.apps,
+  Icons.quick_contacts_mail,
+  Icons.web,
+];
