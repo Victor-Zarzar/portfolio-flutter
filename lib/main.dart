b43d1918ed0444add6193e4953b7e0a47db30404
victorzarzar <victorzarzar58@gmail.com>
2024-08-14 16:00:16 -0300

    Feat: add provider/sharedprerences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter/pages/HomePage/home_page.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:portfolio_flutter/provider/DarkTheme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,19 +20,33 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Portfolio - Victor Zarzar",
-      theme: ThemeData.dark(),
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: const HomePage(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => UiProvider()..init(),
+      child: Consumer<UiProvider>(
+        builder: (context, UiProvider notifier, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Portfolio - Victor Zarzar",
+            themeMode: notifier.isDark ? ThemeMode.dark : ThemeMode.light,
+            darkTheme:
+                notifier.isDark ? notifier.darkTheme : notifier.lightTheme,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            home: const HomePage(),
+          );
+        },
+      ),
     );
   }
 }
--- lib/pages/HomePage/home_page.dart
+++ lib/pages/HomePage/home_page.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter/constants/Colors/colors.dart';
 import 'package:portfolio_flutter/constants/Size/size.dart';
+import 'package:portfolio_flutter/provider/DarkTheme/app_theme.dart';
 import 'package:portfolio_flutter/widgets/ContactSection/contact_section.dart';
 import 'package:portfolio_flutter/widgets/DrawerMobile/drawer_mobile.dart';
 import 'package:portfolio_flutter/widgets/Footer/footer.dart';
@@ -12,6 +13,7 @@ import 'package:portfolio_flutter/widgets/NavMobile/nav_mobile.dart';
 import 'package:portfolio_flutter/widgets/ProjectsSection/projects_section.dart';
 import 'package:portfolio_flutter/widgets/SkillsDesktop/skills_desktop.dart';
 import 'package:portfolio_flutter/widgets/SkillsMobile/skills_mobile.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -44,82 +46,90 @@ class _HomePageState extends State<HomePage> {
     final screenWidth = screenSize.width;
 
     return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        key: scaffoldKey,
-        backgroundColor: CustomColor.scaffoldBg,
-        endDrawer: constraints.maxWidth >= kMinDesktopWidth
-            ? null
-            : DrawerMobile(onNavItemTap: (int navIndex) {
-                scaffoldKey.currentState?.closeEndDrawer();
-                scrollToSection(navIndex);
-              }),
-        body: SingleChildScrollView(
-          controller: scrollController,
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              SizedBox(key: navbarKeys.first),
-              // MAIN //
-              if (constraints.maxWidth >= kMedDesktopWidth)
-                NavDesktop(onNavMenuTap: (int navIndex) {
+      return Consumer<UiProvider>(builder: (context, notifier, child) {
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: notifier.isDark
+              ? CustomColor.scaffoldBg
+              : CustomColor.whitePrimary,
+          endDrawer: constraints.maxWidth >= kMinDesktopWidth
+              ? null
+              : DrawerMobile(onNavItemTap: (int navIndex) {
+                  scaffoldKey.currentState?.closeEndDrawer();
                   scrollToSection(navIndex);
-                })
-              else
-                NavMobile(
-                  onLogoTap: () {},
-                  onMenuTap: () {
-                    scaffoldKey.currentState?.openEndDrawer();
-                  },
-                ),
-              if (constraints.maxWidth >= kMinDesktopWidth)
-                const MainDesktop()
-              else
-                const MainMobile(),
-              // SKILLS //
-              Container(
-                key: navbarKeys[1],
-                width: screenWidth,
-                padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
-                color: CustomColor.bgLight1,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // title
-                    Text(
-                      'skills'.tr(),
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: CustomColor.whitePrimary,
+                }),
+          body: SingleChildScrollView(
+            controller: scrollController,
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                SizedBox(key: navbarKeys.first),
+                // MAIN //
+                if (constraints.maxWidth >= kMedDesktopWidth)
+                  NavDesktop(onNavMenuTap: (int navIndex) {
+                    scrollToSection(navIndex);
+                  })
+                else
+                  NavMobile(
+                    onLogoTap: () {},
+                    onMenuTap: () {
+                      scaffoldKey.currentState?.openEndDrawer();
+                    },
+                  ),
+                if (constraints.maxWidth >= kMinDesktopWidth)
+                  const MainDesktop()
+                else
+                  const MainMobile(),
+                // SKILLS //
+                Container(
+                  key: navbarKeys[1],
+                  width: screenWidth,
+                  padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+                  color: notifier.isDark
+                      ? CustomColor.scaffoldBg
+                      : CustomColor.whitePrimary,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // title
+                      Text(
+                        'skills'.tr(),
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: notifier.isDark
+                              ? CustomColor.textFieldBg
+                              : CustomColor.hintDark,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 50),
+                      const SizedBox(height: 50),
 
-                    // platforms and skills
-                    if (constraints.maxWidth >= kMedDesktopWidth)
-                      const SkillsDesktop()
-                    else
-                      const SkillsMobile(),
-                  ],
+                      // platforms and skills
+                      if (constraints.maxWidth >= kMedDesktopWidth)
+                        const SkillsDesktop()
+                      else
+                        const SkillsMobile(),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              // PROJECTS //
-              ProjectsSection(
-                key: navbarKeys[2],
-              ),
-              const SizedBox(height: 30),
-              // CONTACT //
-              ContactSection(
-                key: navbarKeys[3],
-              ),
-              const SizedBox(height: 30),
-              // FOOTER //
-              const Footer(),
-            ],
+                const SizedBox(height: 30),
+                // PROJECTS //
+                ProjectsSection(
+                  key: navbarKeys[2],
+                ),
+                const SizedBox(height: 30),
+                // CONTACT //
+                ContactSection(
+                  key: navbarKeys[3],
+                ),
+                const SizedBox(height: 30),
+                // FOOTER //
+                const Footer(),
+              ],
+            ),
           ),
-        ),
-      );
+        );
+      });
     });
   }
 }
